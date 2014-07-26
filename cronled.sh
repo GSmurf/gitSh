55668ac2ff090f3b1aa6be8b68ec6340aaa9093b
GiSmo <gsmurf7@gmail.com>
2014-07-26 13:17:14 +0200

    Voyant rouge si plus de deux connexion ssh

--- cronled.sh
+++ cronled.sh
@@ -14,17 +14,15 @@ date=$(date '+%Y-%m-%d %H:%M:%S')
 # Recupere l'heure de la derniere connexion qui ne soit pas du à 127.0.0.1 sur les 50 derniers acces d'apache
 lastConnection=$(tail -n 50 /var/log/apache2/access.log | grep -v 127.0.0 | awk '{print substr($4,14,length($4))}' | tail -n 1)
 
-case $nb in
-  1)
+if [ $nb = 1 ]
+then
 	# si un utilisateur est connecté au ssh allume le gpio 2 (rouge) 
 	sudo gpio write 1 1
 	echo "$date : 1 connexion SSH, derniere connexion à apache : $lastConnection"
-	;;
-  2)
+else if [ $nb > 1 ]
+then
 	# si un utilisateur est connecté au ssh allume le gpio 1 (jaune) 
 	sudo gpio write 2 1
 	echo "$date : 2 connexions SSH, derniere connexion à apache : $lastConnection"
-	;;
-  *)
-	;;
-esac
+fi
+fi
