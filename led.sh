105b27e6f7a736e85037000266fb97a33027ccf5
GiSmo <gsmurf7@gmail.com>
2014-08-03 18:44:53 +0200

    Ajout d'un scipt de timing de clignotement des leds

---
+++ led.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# initialise la led avec le premier parametre
+# par du status que la led est au debut etteinte l'allume x sec du parametre 1
+# puis l'etteinds x sec du parametre 2 etc ...
+
+
+# ------------------------------------------------
+# Fonctions
+# ------------------------------------------------
+function initLed {
+        for i in $verte $jaune $rouge
+        do
+          sudo gpio mode $i out
+        done
+}
+
+function allumeLed {
+  sudo gpio write $1 1
+#  echo "led $1 allumée"
+}
+
+function eteindLed {
+  sudo gpio write $1 0
+#  echo "led $1 eteinte"
+}
+
+# ------------------------------------------------
+# Main
+# ------------------------------------------------
+
+# initialise la led avec le premier parametre
+initLed $1
+
+j=1
+allume=false
+for i in "$@"
+do
+ if [ $j != 1 ]
+ then
+   if [ $allume = false ]
+   then
+     # par du status que la led est au debut etteinte l'allume x sec du parametre 1
+     allumeLed $1
+     allume=true
+     sleep $i
+   else
+     # puis l'etteinds x sec du parametre 2 etc ...
+     eteindLed $1
+     allume=false
+     sleep $i
+   fi
+ fi
+ j=j+1
+done
