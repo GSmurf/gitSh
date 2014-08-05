bdf8c96358e8288bb5c9079bcccf5d11dfaa6f2e
GiSmo <gsmurf7@gmail.com>
2014-08-05 14:04:15 +0200

    Ajout de la possiblité d'envoyer un sms avec un texte personnalisé

--- sms.sh
+++ sms.sh
@@ -1,9 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 . /home/pi/gitSh/sms_parametres.sh
 
 date=$(date '+%A %d/%m/%Y à %H:%M:%S')
-message="Sms envoyé depuis mon Raspberry Pi le $date"
+
+# si le parametre 1 est --help alors affiche l'aide
+if [ "$1" = "--help" ]
+then
+  echo "sms: Aide"
+  echo "Saisissez « ./sms.sh 'Envoi du sms de test' » pour faire fonctionner ce script."
+  exit
+fi
+
+# Utilise le parametre 1 comme message s'il est transmit
+if [ $# = 1 ] 
+then
+  message="$1 $date"
+else
+  message="Sms envoyé depuis mon Raspberry Pi le $date"
+fi
 
 # execute l'envoi de sms avec l'api de free
 curl -s -i -k "https://smsapi.free-mobile.fr/sendmsg?user=$user&pass=$pass&msg=$message" > tmp.log
