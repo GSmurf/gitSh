b20de85063e95f9277bbd288140d0efb3cf224c7
GiSmo <gsmurf7@gmail.com>
2014-08-05 14:13:47 +0200

    Renommage du script de demarrage et ajout de l'envoi d'un sms le notifiant

--- demarrage.sh
+++ demarrage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./sms.sh 'Je suis ton Raspberry Pi pres à servir ;)' &
+
 ./led.sh 0 1 0.1 1 0.1 1 0.1 1 0.1 1 0.1 &
 ./led.sh 1 0.2 0.4 1 0.4 1 0.4 2 0.1 &
 ./led.sh 2 0.3 0.3 0.3 0.3 0.3 3.3 0.5 0.2 &
